cf5b087d6a810fac7c2cdfde810d6b385a277d56
lupengliang <1559540409@qq.com>
2021-12-16 22:38:18 +0800

    2021-12-16 用户应用 && 笔记应用新增命名空间、路由别名

--- tedu_note/note/urls.py
+++ tedu_note/note/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 
 from . import views
 
-
+app_name = 'note'  # 添加应用命名空间
 urlpatterns = [
-    path('add', views.add_note),
-    path('all', views.show_note),
-    path('update/<int:note_id>', views.update_note),
-    path('delete', views.delete_note),
+    path('add/', views.add_note, name='add_note'),  # 重命名可以优化模板中的硬编码
+    path('all/', views.show_note, name='show_note'),
+    path('update/<int:note_id>/', views.update_note, name='update_note'),
+    path('delete/', views.delete_note, name='delete_note'),
 ]
--- tedu_note/user/urls.py
+++ tedu_note/user/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'user'
 urlpatterns = [
-    path('reg', views.reg_view),
-    path('login', views.login_view),
-    path('logout', views.logout_view),
+    path('reg', views.reg_view, name='reg_view'),
+    path('login', views.login_view, name='login_view'),
+    path('logout', views.logout_view, name='logout_view'),
 ]
