9f81108ddbdbc96a4bf3fcc807313451922c8b48
lupengliang <1559540409@qq.com>
2021-12-19 21:48:01 +0800

    2021-12-19 新增测试文件规划

--- tedu_note/note/tests.py
+++ tedu_note/note/tests.py
@@ -1,3 +1,53 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+# 用来笔记增删改查功能
+class NoteTests(TestCase):
+    """
+    用来笔记增删改查功能
+    """
+
+    # 测试创建笔记功能
+    def test_create_note(self):
+        """
+        测试创建一条笔记功能
+        :version: 1.0
+        :author: lupengliang
+        :date: 2021-12-19
+        :return:
+        """
+        pass
+
+    # 测试更新一条笔记功能
+    def test_update_note(self):
+        """
+        测试更新一条笔记功能
+        :version: 1.0
+        :author: lupengliang
+        :date: 2021-12-19
+        :return:
+        """
+        pass
+
+    # 测试查询笔记功能
+    def test_select_note(self):
+        """
+        测试查询笔记功能
+        :version: 1.0
+        :author: lupengliang
+        :date: 2021-12-19
+        :return:
+        """
+        pass
+
+    # 测试删除一条笔记功能
+    def test_delete_note(self):
+        """
+        测试删除一条笔记功能
+        :version: 1.0
+        :author: lupengliang
+        :date: 2021-12-19
+        :return:
+        """
+        pass
--- tedu_note/user/tests.py
+++ tedu_note/user/tests.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+# 用来测试用户登录、注册
+class UserTests(TestCase):
+    """
+    用来测试用户登录、注册
+    """
+
+    # 测试注册用户功能
+    def test_register_user(self):
+        """
+        测试注册用户功能
+        :version: 1.0
+        :author: lupengliang
+        :date: 2021-12-19
+        :return:
+        """
+        response = self.client.get(reverse('user:reg_view'))
+        print(response)
+
+    # 测试登录功能
+    def test_login_function(self):
+        """
+        测试登录功能
+        :version: 1.0
+        :author: lupengliang
+        :date: 2021-12-19
+        :return:
+        """
+        pass
+
+    # 测试退出功能
+    def test_logout_function(self):
+        """
+        测试退出功能
+        :author: lupengliang
+        :date: 2021-12-19
+        :return:
+        """
+        pass
