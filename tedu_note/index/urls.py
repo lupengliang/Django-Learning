ee651467d2bab815ee0bbad337cece463d22cc62
lupengliang <1559540409@qq.com>
2022-02-16 23:09:09 +0800

    2022.02.16 美化云笔记首页

--- tedu_note/index/urls.py
+++ tedu_note/index/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'index'
 urlpatterns = [
-    path('index', views.index_view),
+    path('index', views.index_view, name='index_view'),
 ]
